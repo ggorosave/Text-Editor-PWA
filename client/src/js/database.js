87372edd8a6b573b45005afbc41b3827936a194f
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-02-02 11:46:19 -0700

    Added method that gets all content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -31,6 +31,22 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // console.error('getDb not implemented')
+  console.log('GET from the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('Data: ', result);
+  return result;
+};
 
 initdb();
